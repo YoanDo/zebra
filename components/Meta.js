a35787b27597e7cc410f76448ffa6e456bc21a9f
Yoan <y.n.dorny@gmail.com>
2019-09-29 21:33:14 +0200

    base setup

---
+++ components/Meta.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <link rel="shortcut icon" href="/static/favicon.png" /> //todo add favicon
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <title>735 | Les zébres crystalins</title>
+  </Head>
+)
+
+export default Meta;
---
+++ components/Page.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
+// import Header from '../components/Header';
+import Meta from '../components/Meta';
+
+
+// variables
+const theme = {
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightGrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  basicText: '1.4rem',
+};
+
+const StyledPage = styled.div`
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 0 2rem;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  html{
+    font-size: 62.5%;
+    box-sizing: border-box;
+    color: ${theme.black}
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body{
+    @import url('https://fonts.googleapis.com/css?family=Space+Mono&display=swap');
+    padding:0;
+    margin: 0;
+    font-size: 1.2rem;
+    line-height: 2;
+    font-family: 'Space Mono', monospace;
+  }
+  a{
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`
+class Page extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Inner>
+            {this.props.children}
+          </Inner>
+        </StyledPage>
+        <GlobalStyle whiteColor />
+      </ThemeProvider>
+    );
+  }
+}
+export default Page;
